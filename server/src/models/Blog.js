e712380987f4158dddc9298ebc619f1d2bb4828e
yunjaeeun44 <yunmegan44@sookmyung.ac.kr>
2022-12-05 02:29:03 +0900

    Chore/#11 conflict 해결

--- server/src/models/Blog.js
+++ server/src/models/Blog.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const BlogSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId, //default
+    _id: mongoose.Schema.Types.ObjectId, // default
     user_id: { 
         type: String, //변경 가능
         required: true
