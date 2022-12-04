25172b1ee5f87264025c471aa989ef574580d30f
yunjaeeun44 <yunmegan44@sookmyung.ac.kr>
2022-12-04 17:13:21 +0900

    Fix/#4 오류해결

--- server/src/service/blogService.js
+++ server/src/service/blogService.js
@@ -1,8 +1,10 @@
 import Blog from "../models/Blog.js";
+import mongoose from 'mongoose';
 
 
 const updateBlog = async (blogId, title, image, content) => {
     try{
+        blogId = mongoose.Types.ObjectId(blogId);
         const updateData = {
             title: title,
             image: image, //이미지 업데이트 기능 추가
