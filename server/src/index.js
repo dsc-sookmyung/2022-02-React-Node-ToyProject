94a74ebedefa47c673f4375e7b89805c0cedb052
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2022-12-06 21:55:06 +0900

    Feat : header에 cors에러 해결을 위해 미들웨어 추가

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import router from "./router/index.js";
 import config from "./config/index.js";
 import connectDB from "./Loaders/db.js";
+import cors from "cors";
 
 const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
